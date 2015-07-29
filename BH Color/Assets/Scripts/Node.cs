1ed2f7fd9814b9f620aa99e7675a391ce3d33627
Mtony360 <Mtony360@gmail.com>
2015-07-29 13:38:18 -0500

    project

---
+++ BH Color/Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+public 	virtual bool run ()
+{
+		return false;
+
+}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
